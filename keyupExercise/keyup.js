4bf9e73013c309f75734dfdc6c1c8a6bbf6f3764
David Juarez <davidjuarez@Davids-Air.attlocal.net>
2020-04-08 14:41:02 -0400

    keyup files

---
+++ keyupExercise/keyup.js
@@ -0,0 +1,23 @@
+// function dataFromKeyup() {
+    var keyup = document.querySelector("input[name='upKey']")
+    keyup.addEventListener('keyup',function(event){
+        // console.log(event);
+        var inputText = document.querySelector("input[name='upKey']").value;
+        
+        console.log(myDiv);
+        var appendToDiv = document.querySelector('#id')
+        
+    })
+
+
+
+
+// document.querySelector("input[name='upKey']").onkeyup = function() {
+//     inputValue()
+// }
+
+// function inputValue(){
+//     var value = document.querySelector('input').value;
+    
+//     console.log(value);
+// }
