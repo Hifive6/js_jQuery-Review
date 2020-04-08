d36e027cf50fa00e2804aed9d4a3e88524c3f116
David Juarez <davidjuarez@Davids-Air.attlocal.net>
2020-04-08 10:49:11 -0400

    blank target append.js

--- append/append.js
+++ append/append.js
@@ -1,4 +1,5 @@
 // $('body').append('<a>').attr('href=https://titus.techtalentsouth.com/');
 
 $('body').append("<a>Click Here");
-$('a').attr('href', 'https://titus.techtalentsouth.com/');
+// $('a').attr('href', 'https://titus.techtalentsouth.com/');
+$('a').attr({'href': 'https://titus.techtalentsouth.com/', 'target': '_blank'});
