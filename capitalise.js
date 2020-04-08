d8da9516d60702616e5ad207feb12e0b2923134e
David Juarez <davidjuarez@Davids-Air.attlocal.net>
2020-04-08 09:47:45 -0400

    map function

--- capitalise.js
+++ capitalise.js
@@ -1,10 +1,12 @@
 strings = ['case', 'book', 'picture', 'buddha'];
 
 function capitaliseWords(){
-    var string = strings.map();
+    var string = strings.map(function(i, string){
+        return i.toUpperCase();
+    });
     // string.toUppercase();
     console.log(string);
-    return string;
+    // return string;
 }
 
 capitaliseWords();
