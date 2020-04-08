c7dfecce79826406e5061dea3e941a29dfe0deb9
David Juarez <davidjuarez@Davids-Air.attlocal.net>
2020-04-08 09:53:46 -0400

    firstletter function

--- capitalise.js
+++ capitalise.js
@@ -3,10 +3,19 @@ strings = ['case', 'book', 'picture', 'buddha'];
 function capitaliseWords(){
     var string = strings.map(function(i, string){
         return i.toUpperCase();
+        
     });
     // string.toUppercase();
     console.log(string);
     // return string;
 }
 
+function capitaliseFirstLetter(){
+    var firstLetter = strings.map(function(i){
+        return i.charAt(0).toUpperCase() + i.slice(1);
+    })
+    console.log(firstLetter);
+}
+
 capitaliseWords();
+capitaliseFirstLetter();
