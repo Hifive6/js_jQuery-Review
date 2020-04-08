d9aa2a9a7adb805bfb4ae44548a38aad755a0eaa
David Juarez <davidjuarez@Davids-Air.attlocal.net>
2020-04-08 09:05:18 -0400

    pizza js

---
+++ pizza.js
@@ -0,0 +1,8 @@
+toppings = ['pepperoni', 'ham', 'bacon', 'sausage', 'green peppers'];
+
+function pizzaToppings(){
+    console.log(toppings)
+    console.log('Pizza topping 1: ' + toppings[1])
+    console.log('A delicious pizza with ' + toppings[0] + ' and ' + toppings[1]+ ' and ' + toppings[2]+ ' and ' + toppings[3]+ ' and ' + toppings[4])
+}
+pizzaToppings();
