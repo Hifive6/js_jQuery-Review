fa29dee5ca5211350c3cb7bc4628b8ccb9536d3b
David Juarez <davidjuarez@Davids-Air.attlocal.net>
2020-04-08 13:17:22 -0400

    it turns reds in make it red

--- makeItRed/red.js
+++ makeItRed/red.js
@@ -1,5 +1,24 @@
 $('span').css('color', 'Red');
 
-$('p').click(function(){
-    $(this).css('color', 'red');
+// $('p').click(function(){
+//     $(this).css('color', 'red');
+// });
+
+
+
+$('p').on('click', function(){
+    var self = this;
+    $(self).addClass('onRed').css('color','red');
+    // $(self).addClass('onRed').css('color','red').toggle();
+    // $(self).removeClass('onRed')
+    
+    // .css('color','red');
+    // $(this).css('color', 'red');
 });
+// $('p').on('click', function(){
+//     var self = this;
+//     // $(self).addClass('onRed').css('color','red');
+//     $(self).removeClass('onRed')
+//     // .css('color','red');
+//     // $(this).css('color', 'red');
+// });
