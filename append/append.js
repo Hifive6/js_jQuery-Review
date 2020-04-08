f8b2443c164d55b16a45cc06d988c5519a1cccca
David Juarez <davidjuarez@Davids-Air.attlocal.net>
2020-04-08 11:07:45 -0400

    make it red

--- append/append.js
+++ append/append.js
@@ -1,5 +1,8 @@
 // $('body').append('<a>').attr('href=https://titus.techtalentsouth.com/');
 
-$('body').append("<a>Click Here");
+    $('body').append("<a>Click Here")
 // $('a').attr('href', 'https://titus.techtalentsouth.com/');
-$('a').attr({'href': 'https://titus.techtalentsouth.com/', 'target': '_blank'});
+$('a').attr({'href': 'https://titus.techtalentsouth.com/', 'target': '_blank'})
+
+
+
