aafa5517f77e922301500853280be4a799ddddc8
David Juarez <davidjuarez@Davids-Air.attlocal.net>
2020-04-08 08:57:17 -0400

    sandwiches made

--- sandWichCalculator.js
+++ sandWichCalculator.js
@@ -1,11 +1,18 @@
 function sandWichCalculator(slicesOfBread, slicesOfCheese){
     var sandwiches = slicesOfBread / 2;
 
-    if((slicesOfBread > 1 ) && (slicesOfCheese < sandwiches)){
+    if((slicesOfBread > 1 ) && (slicesOfCheese < sandwiches) && (slicesOfCheese < sandwiches)){
         sandwiches = slicesOfCheese;
         console.log(sandwiches);
         return sandwiches
+    } else if((slicesOfCheese >= sandwiches) && (slicesOfBread > 1)) {
+        console.log(sandwiches);
+        return sandwiches;
+    } else if((slicesOfCheese = 0) || (slicesOfBread < 2)){
+        console.log("none for you")
+        return 0;
     }
+
 }
 
-sandWichCalculator(10);
+sandWichCalculator(2,5);
