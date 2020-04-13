956aa689f05c69e5bdb9444ea26f76321de5d757
David Juarez <djuar05@gmail.com>
2020-04-13 18:07:49 -0400

    logic and keeping score

--- guessingGame/guess.js
+++ guessingGame/guess.js
@@ -1,16 +1,30 @@
 function playerGuess(){
+    let guesses = 10;
+    console.log('you have ' + guesses + ' guesses left');
     randomGuess = Math.floor(Math.random()*100)
     console.log(randomGuess);
+
     $('#inputBtn').click(function(){
-        // console.log("pushed")
+        
         userGuess = $('#userInput').val().trim();
         console.log(userGuess);
+
         if (userGuess < randomGuess){
-            console.log('guess is too low');
+            console.log('guess is too low, Try Again');
+            guesses --;
+            console.log('you have ' + guesses + ' guesses left')
             $('#userInput').val('');
+            if(guesses < 1){
+                console.log('You have run out of guesses, Sorry')
+                return false;
+            }
+
         } else if (userGuess > randomGuess){
-            console.log("guess is to high")
+            console.log("guess is to high, Try Again");
+            guesses --;
+            console.log('you have ' + guesses + ' guesses left');
             $('#userInput').val('');
+
         } else {
             console.log("guess on the nose")
             $('#userInput').val('');
